480fbecadf2214c167e56b4458e542f2931d7e63
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-06-17 12:22:39 -0700

    code for connection to local mongoDB

--- app.js
+++ app.js
@@ -17,15 +17,22 @@ var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
 
-mongoose.connect(process.env.DATABASEURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
+// mongoose.connect(process.env.DATABASEURL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to DB!'))
+// .catch(error => console.log(error.message));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
+mongoose.connect('mongodb://localhost/yelpCampDB', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(() => console.log('Connected to DB!'))
+	.catch(error => console.log(error.message));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
@@ -34,8 +41,8 @@ app.use(flash());
 //Passport Configuration
 app.use(require("express-session")({
 	secret: "Toto is positive",
-	resave:false,
-	saveUninitialized:false
+	resave: false,
+	saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,18 +50,18 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
 	res.locals.currentUser = req.user;
 	res.locals.error = req.flash("error");
 	res.locals.success = req.flash("success");
 	next();
 })
 
-app.use("/",indexRoutes);
-app.use("/campgrounds",campgroundRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log("Server Has Started!");
+	console.log("Server Has Started!");
 });
